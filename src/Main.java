a9c95bd30943e375a59fc5d39358d426ec3679d8
dumitruhojda <dumitruhojda09@gmail.com>
2019-02-11 18:00:19 +0200

    Homework Nu stiu cat este de bine ,dar invat :D

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,20 @@
 public class Main {
-
-
-
-
 	public static void main(String[] args) {
         System.out.println("Hello!");
 //1. Write a Java program to print 'Hello' on screen and then print your name on a separate line
         System.out.println(" Dumitru Hojda ");
 //2. Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the sum of the two numbers
+      short=7;
+      short=8;
         System.out.println("The Adition of 7+8 is 15");
 
 
 //3. Write a Java program to divide two numbers and print on the screen.
+		int= (50);
 		System.out.println("The division of 50 is: 1,2,5,10,15,50");
+		int=(16);
 		System.out.println("The division of 16 is: 1,2,4,8,16");
-				//4. Write a Java program to print the result of the following operations:
+		//4. Write a Java program to print the result of the following operations:
 				System.out.println(-5 + 8 * 6);
 		        System.out.println((55+9) % 9);
                 System.out.println(20 + -3*5 / 8);
