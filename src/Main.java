98bf50c8d75ed91eb56496fc8f3d6ce8cc50c68c
dumitruhojda <dumitruhojda09@gmail.com>
2019-02-11 14:58:54 +0200

    Homework

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!");
+//1. Write a Java program to print 'Hello' on screen and then print your
+//name on a separate line
         System.out.println(" Dumitru Hojda ");
-    }}
+//2. Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the
+//sum of the two numbers
+        System.out.println("7+8");
+        //3. Write a Java program to divide two numbers and print on the screen.
+
+				//4. Write a Java program to print the result of the following operations:
+				System.out.println(-5 + 8 * 6);
+		System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);}
+    }
