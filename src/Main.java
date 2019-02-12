9d29a972a6844e2fe7588d402af70a284251393d
dumitruhojda <dumitruhojda09@gmail.com>
2019-02-12 22:58:50 +0200

    Modificare

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
-        System.out.println("Hello!");
+		System.out.println("Hello!");
 //1. Write a Java program to print 'Hello' on screen and then print your name on a separate line
-        System.out.println(" Dumitru Hojda ");
+		System.out.println(" Dumitru Hojda ");
 //2. Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the sum of the two numbers
-      short=7;
-      short=8;
-        System.out.println("The Adition of 7+8 is 15");
+		int x = 7;
+		int y = 8;
+		System.out.println("The Adition of 7+8 is: "+(x+y));
+	//3. Write a Java program to divide two numbers and print on the screen.
+
+			Scanner input = new Scanner (System.in);
+			System.out.print("Input the first number: 7");
+			int a = input.nextInt();
+			System.out.print("Input the second number: 8 ");
+			int b = input.nextInt();
+			int d = (a/b);
+			System.out.println();
+			System.out.println("The division of a and b is:" +d);
 
 
-//3. Write a Java program to divide two numbers and print on the screen.
-		int= (50);
-		System.out.println("The division of 50 is: 1,2,5,10,15,50");
-		int=(16);
-		System.out.println("The division of 16 is: 1,2,4,8,16");
-		//4. Write a Java program to print the result of the following operations:
-				System.out.println(-5 + 8 * 6);
-		        System.out.println((55+9) % 9);
-                System.out.println(20 + -3*5 / 8);
-                System.out.println(5 + 15 / 3 * 2 - 8 % 3);}
-    }
 
+	}}
