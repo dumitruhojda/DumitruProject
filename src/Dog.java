42a4020142bcb671917028851d34ae93fb356903
dumitruhojda <dumitruhojda09@gmail.com>
2019-02-13 21:02:49 +0200

    Dog + Fetita

---
+++ src/Dog.java
@@ -0,0 +1,15 @@
+public class Dog {
+	String culoare;
+	int varsta;
+	Float greutate ;
+	String rase;
+	String name;
+	String comportamente;
+	public void alearga() {
+		System.out.println("Catelul musca");
+	}
+public void mananca(){System.out.println("Momentan,mananca niste granule ");
+	}
+public void latra (){System.out.println("hahah");
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -2,24 +2,44 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello!");
+		//System.out.println("Hello!");
 //1. Write a Java program to print 'Hello' on screen and then print your name on a separate line
-		System.out.println(" Dumitru Hojda ");
+		//System.out.println(" Dumitru Hojda ");
 //2. Write a Java program to print ‘The addition of 7 and 8 is: ‘, and then the sum of the two numbers
-		int x = 7;
-		int y = 8;
-		System.out.println("The Adition of 7+8 is: "+(x+y));
-	//3. Write a Java program to divide two numbers and print on the screen.
+		//int x = 7;
+		//int y = 8;
+		//System.out.println("The Adition of 7+8 is: "+(x+y));
+		//3. Write a Java program to divide two numbers and print on the screen.
 
-			Scanner input = new Scanner (System.in);
-			System.out.print("Input the first number: 7");
-			int a = input.nextInt();
-			System.out.print("Input the second number: 8 ");
-			int b = input.nextInt();
-			int d = (a/b);
-			System.out.println();
-			System.out.println("The division of a and b is:" +d);
+		//Scanner input = new Scanner (System.in);
+		//System.out.print("Input the first number: ");
+		//int a = input.nextInt();
+		//System.out.print("Input the second number: ");
+		//int b = input.nextInt();
+		//int d = (a/b);
+		//System.out.println();
+		//System.out.println("The division of a and b is:" +d);
+//		int res = scadere(2, 3);
+//		System.out.println(res);
+		Scanner scanner = new Scanner((System.in));
+		int x = scanner.nextInt();
+		String y = scanner.next();
 
+        Dog Scooby = new Dog();
 
+     System.out.print("numele este" + "Introdu culoarea");
 
-	}}
+     Scooby.name = "Scooby";
+     Scooby.culoare = "Albastru";
+     Scooby.rase = "Bishon";
+     Scooby.varsta = 3;
+System.out.println("numele este:"+Scooby.name);
+
+	}
+
+//	public static int scadere(int primul , int alDoileaNumar)
+//		int res = primul-alDoileaNumar;
+//		return res;
+
+	}
+
